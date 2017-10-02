ab890e988a5d56896fb28bfa1594b785bf2141a1
Taylor Slye <slyet@oregonstate.edu>
2017-10-02 09:50:24 -0700

    Two circles, with colour

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 // Zombulator by Taylor Slye
 function setup() {
-
+	createCanvas(800, 800);
 }
 
 function draw() {
-
+	fill(255, 0, 0);
+	ellipse(50, 50, 80, 80);
+	fill(150, 150, 200);
+	ellipse(200, 100, 80, 80);
 }
