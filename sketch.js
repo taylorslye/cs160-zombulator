bbf633b229ffd3487a3be3715e93a1bd13194251
Taylor Slye <slyet@oregonstate.edu>
2017-10-04 09:48:03 -0700

    Made the circles move

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 // Zombulator by Taylor Slye
+
+var zombiex = 50;
+var zombie2x = 100;
 function setup() {
 	createCanvas(800, 800);
 }
 
 function draw() {
+	background(255, 255, 255);
 	fill(255, 0, 0);
-		ellipse(50, 50, 80, 80);
+		ellipse(zombiex, zombiex, 80, 80);
 	fill(135, 224, 123);
-		ellipse(200, 100, 80, 80);
+		ellipse(zombie2x, 100, 80, 80);
+		zombiex = zombiex + .1;
+		zombie2x = zombie2x +.2;
 }
