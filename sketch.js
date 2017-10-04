17031fcac0871a0517fa012f3c61a69edd5c5d90
Taylor Slye <slyet@oregonstate.edu>
2017-10-04 09:17:13 -0700

    formatted to look better

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
 
 function draw() {
 	fill(255, 0, 0);
-	ellipse(50, 50, 80, 80);
+		ellipse(50, 50, 80, 80);
 	fill(150, 150, 200);
-	ellipse(200, 100, 80, 80);
+		ellipse(200, 100, 80, 80);
 }
