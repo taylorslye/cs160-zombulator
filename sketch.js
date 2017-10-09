b23acc2f4ab4ee75a03be47370e343e00abfcaec
Taylor Slye <slyet@oregonstate.edu>
2017-10-08 21:33:15 -0700

    made circle speed up and change colors when bouncing on walls

--- sketch.js
+++ sketch.js
@@ -2,16 +2,33 @@
 
 var zombiex = 50;
 var zombie2x = 100;
+var bouncing = 1;
+var colour = 255;
+var colour2 = 0;
 function setup() {
 	createCanvas(800, 800);
 }
 
 function draw() {
 	background(255, 255, 255);
-	fill(255, 0, 0);
+	fill(colour, colour2, 0);
 		ellipse(zombiex, zombiex, 80, 80);
 	fill(135, 224, 123);
 		ellipse(zombie2x, 100, 80, 80);
-		zombiex = zombiex + .1;
-		zombie2x = zombie2x +.2;
+		zombiex = zombiex + bouncing;
+		zombie2x = zombie2x +2;
+	if (zombiex >= 800) {
+		bouncing = -1.5*bouncing;
+		colour = 0;
+		colour2 = 255;
+	}
+	 if (zombie2x >= 800) {
+		zombie2x = 0;
+	 }
+	 if (zombiex <= 0) {
+	 	bouncing = -1.5 * bouncing;
+	 	colour = 255;
+		colour2 = 0;
+	 }
 }
+
